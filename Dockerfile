9251a129adb8716396e2f93dcfce4020ae5cc50f
Suryanto Tan <suryanto.tan5@gmail.com>
2022-10-19 22:20:01 +0700

    add pdo dan setup database

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM php:8.0-apache
 
 EXPOSE 80
+RUN apt-get update
+RUN apt-get install -y \
+  libpq-dev
 
+RUN docker-php-ext-install pdo pdo_pgsql
 COPY src/ /var/www/html/
--- database.sql
+++ database.sql
@@ -26,12 +26,12 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.Album (
     album_id integer NOT NULL,
-    "Judul" character varying(64) NOT NULL,
-    "Penyanyi" character varying(128) NOT NULL,
-    "Total_duration" integer NOT NULL,
-    "Image_path" character varying(256) NOT NULL,
-    "Tanggal_Terbit" date NOT NULL,
-    "Genre" character varying(64)
+    Judul character varying(64) NOT NULL,
+    Penyanyi character varying(128) NOT NULL,
+    Total_duration integer NOT NULL,
+    Image_path character varying(256) NOT NULL,
+    Tanggal_Terbit date NOT NULL,
+    Genre character varying(64)
 );
 
 
@@ -43,13 +43,13 @@ ALTER TABLE public.Album OWNER TO postgres;
 
 CREATE TABLE public.Song (
     song_id integer NOT NULL,
-    "Judul" character varying(64) NOT NULL,
-    "Penyanyi" character varying(128),
-    "Tanggal_terbit" date NOT NULL,
-    "Genre" character varying(64),
-    "Duration" integer NOT NULL,
-    "Audio_path" character varying(256) NOT NULL,
-    "Image_path" character varying(256) NOT NULL,
+    Judul character varying(64) NOT NULL,
+    Penyanyi character varying(128),
+    Tanggal_terbit date NOT NULL,
+    Genre character varying(64),
+    Duration integer NOT NULL,
+    Audio_path character varying(256) NOT NULL,
+    Image_path character varying(256) NOT NULL,
     album_id integer
 );
 
@@ -57,25 +57,25 @@ CREATE TABLE public.Song (
 ALTER TABLE public.Song OWNER TO postgres;
 
 --
--- Name: User; Type: TABLE; Schema: public; Owner: postgres
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.User (
+CREATE TABLE public.user (
     user_id integer NOT NULL,
     email character varying(256) NOT NULL,
     password character varying(256) NOT NULL,
     username character varying(256) NOT NULL,
-    "isAdmin" boolean DEFAULT false NOT NULL
+    isAdmin boolean DEFAULT false NOT NULL
 );
 
 
-ALTER TABLE public.User OWNER TO postgres;
+ALTER TABLE public.user OWNER TO postgres;
 
 --
 -- Data for Name: Album; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.Album (album_id, "Judul", "Penyanyi", "Total_duration", "Image_path", "Tanggal_Terbit", "Genre") FROM stdin;
+COPY public.Album (album_id, Judul, Penyanyi, Total_duration, Image_path, Tanggal_Terbit, Genre) FROM stdin;
 \.
 
 
@@ -83,15 +83,15 @@ COPY public.Album (album_id, "Judul", "Penyanyi", "Total_duration", "Image_path"
 -- Data for Name: Song; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.Song (song_id, "Judul", "Penyanyi", "Tanggal_terbit", "Genre", "Duration", "Audio_path", "Image_path", album_id) FROM stdin;
+COPY public.Song (song_id, Judul, Penyanyi, Tanggal_terbit, Genre, Duration, Audio_path, Image_path, album_id) FROM stdin;
 \.
 
 
 --
--- Data for Name: User; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.User (user_id, email, password, username, "isAdmin") FROM stdin;
+COPY public.user (user_id, email, password, username, isAdmin) FROM stdin;
 1	admin@gmail.com	sayaadmin	admin	t
 \.
 
@@ -105,10 +105,10 @@ ALTER TABLE ONLY public.Album
 
 
 --
--- Name: User User_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: user User_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.User
+ALTER TABLE ONLY public.user
     ADD CONSTRAINT "User_pkey" PRIMARY KEY (user_id);
 
 
