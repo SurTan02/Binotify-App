4b22894046ccd338e79cba49954c36388c011962
Suryanto Tan <suryanto.tan5@gmail.com>
2022-10-22 17:20:24 +0700

    refactor : edit database class

--- src/app/database.php
+++ src/app/database.php
@@ -1,13 +1,36 @@
 <?php
-    $host = "db";
-    $dbuser = "postgres";
-    $dbpass = "postgres";
-    $port = "5432";
-    $dbname = "database";
-    try{
-        $db_connection = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass);
-    } catch ( PDOException $e ) {
-        echo 'ERROR!';
-        print_r( $e );
+    class Database {
+        private $db_connection;
+        private $query;
+
+        public function __construct() {
+            $host = "db";
+            $dbuser = "postgres";
+            $dbpass = "postgres";
+            $port = "5432";
+            $dbname = "database";
+
+            $this->db_connection = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass);
+        }
+
+        public function query($query) {
+            $this->query = $this->db_connection->prepare($query);
+        }
+
+        public function execute() {
+            $res = $this->query->execute();
+            return $res;
+        }
+
+        public function multi_result() {
+            $this->execute();
+            return $this->query->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function single_result() {
+            $this->execute();
+            return $this->query->fetch(PDO::FETCH_ASSOC);
+        }
+                
     }
 ?>
