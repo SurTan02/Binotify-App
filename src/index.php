ed787678e749cd229b2dcb810009683eb5935e08
fikrikhoironn <fikrikf470@gmail.com>
2022-10-22 09:16:15 +0700

    feat: header for admin

--- src/index.php
+++ src/index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Hello World";
+    $header_html = file_get_contents('./view/html/components/header_admin.html');
+    
+    $head_html = file_get_contents('./view/html/templates/head.html');
+    // Replace any variables in the html files.
+    $head_html = str_replace('{title}', 'Spotify', $head_html);
+    $head_html = str_replace('{css1}', './view/css/components/header.css', $head_html);
+    // echo
+    echo $head_html;
+    echo $header_html;
 ?>
