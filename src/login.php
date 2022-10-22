f9161204724ddfb7edd71a95b20e7f31dfa2d45f
fikrikhoironn <fikrikf470@gmail.com>
2022-10-22 07:06:26 +0700

    feat: views for login page

---
+++ src/login.php
@@ -0,0 +1,17 @@
+<?php
+$head_html = file_get_contents('./view/html/templates/head.html');
+
+// Replace any variables in the html files.
+$head_html = str_replace('{title}', 'Login: Spotify', $head_html);
+$head_html = str_replace('{css2}', './css/login.css', $head_html);
+
+// BODY HTML 
+$login_html = file_get_contents('./view/html/login.html');
+$foot_html = file_get_contents('./view/html/templates/foot.html');
+
+// Echo the login page.
+echo $head_html;
+echo $login_html;
+echo '<script src="./js/login.js"></script>';
+echo $foot_html;
+?>
