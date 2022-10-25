00d7fc08116e584877245653e4ab35e515f39d3e
Suryanto Tan <suryanto.tan5@gmail.com>
2022-10-25 23:22:01 +0700

    refactor : edit detail_lagu display page

--- src/detail_lagu.php
+++ src/detail_lagu.php
@@ -1,13 +1,24 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'].'/app/database.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/app/model/Song.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/model/User.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/helper/cookies.php';
 
+    session_start();
+    // Get the user's id.
+    $idUser = validateAuthCookie($_COOKIE);
 
-    $header_html = file_get_contents('./view/html/components/header_admin.html');
+    $user = new User();
+    $isAdmin = $user->getRoleById($idUser);
+    if ($isAdmin) {
+        $header_html = file_get_contents('./view/html/components/header_admin.html');
+    }
+    else {
+        $header_html = file_get_contents('./view/html/components/header_user.html');
+    }
+
     $header_html = str_replace('{css1}', '/view/css/components/header.css', $header_html);
     $header_html = str_replace('{css2}', '/view/css/detail_lagu.css', $header_html);
-    
-    
     
     // BODY HTML 
     $detail_lagu_html = file_get_contents('./view/html/detail_lagu.html');
@@ -18,19 +29,28 @@
             $songId = $_GET['song_id'];
 
             // Get Song with id - song_id
-            $lagu = new Song();
+            $lagu = new Song($songId);
             $result = $lagu->getSongById($songId);
 
             if($result){
                 $header_html = str_replace('{title}', "Brisic - " . $result['judul'], $header_html);
-                
                 $detail_lagu_html = str_replace('{judul_lagu}', $result['judul'], $detail_lagu_html);
                 $detail_lagu_html = str_replace('{penyanyi}', $result['penyanyi'], $detail_lagu_html);
                 $detail_lagu_html = str_replace('{tanggal_terbit}', $result['tanggal_terbit'], $detail_lagu_html);
                 $detail_lagu_html = str_replace('{genre}', $result['genre'], $detail_lagu_html);
-                $detail_lagu_html = str_replace('{audio_path}',  $result['audio_path'] . ".mp3", $detail_lagu_html);
+                $detail_lagu_html = str_replace('{audio_path}',  $result['audio_path'] , $detail_lagu_html);
                 $detail_lagu_html = str_replace('{image_path}',  $result['image_path'] , $detail_lagu_html);
 
+                if ($result['album_id']){
+                    $detail_lagu_html = str_replace('{judul_album}',  $result['judul_album'] , $detail_lagu_html);
+                }else{
+                    $detail_lagu_html = str_replace('{judul_album}',  '' , $detail_lagu_html);
+                }
+            
+                // Edit Config
+                if ($isAdmin){
+                    $foot_html = str_replace('{js1}', './view/js/edit_lagu.js', $foot_html);
+                }
                 $content_page_html =  $detail_lagu_html;
             }
             else{
