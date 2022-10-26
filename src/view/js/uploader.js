1848902c7b0ee2043aabfa148c743279a798a3fb
Suryanto Tan <suryanto.tan5@gmail.com>
2022-10-26 19:42:12 +0700

    feat : add uploader handler

--- src/view/js/uploader.js
+++ src/view/js/uploader.js
@@ -1,12 +1,11 @@
 export function uploadHandler(type) {
     if (type === "image"){
-      var error_path = document.getElementById("error_edit_image_path");
-    //   var files = document.getElementById("edit_image_path").files;
-      var files = document.getElementById("edit_image_path").files;
+      var error_path = document.getElementById("error_image_path");
+      var files = document.getElementById("image_path").files;
       var php_path = "view/js/ajax/upload_album.php";
     }else{
-      var error_path = document.getElementById("error_edit_audio_path");
-      var files = document.getElementById("edit_audio_path").files;
+      var error_path = document.getElementById("error_audio_path");
+      var files = document.getElementById("audio_path").files;
       var php_path = "view/js/ajax/upload_song.php";
     }
   
@@ -19,27 +18,49 @@ export function uploadHandler(type) {
        xhttp.open("POST", php_path, true);
   
        // call on request changes state
+       var response;
        xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
-            var response = this.responseText;
-            
+            response = this.responseText;
+            error_path.innerHTML="";
             if(response == 1){
-            //   console.log(files[0].name);
-            //   alert("Upload successfully.");
-                
+              // alert("Upload successfully.");
             } else if(response == 2){
-              error_path.innerHTML="JPG, JPEG, PNG ONLY";
+              if (type === "image"){
+                error_path.innerHTML="JPG, JPEG, PNG ONLY";
+              } else{
+                error_path.innerHTML="MP3, WAV ONLY";
+              }
             } else{
-              console.log(response);
-              alert("File not uploaded.");
+              // alert("File not uploaded.");
             }
           }
        };
        // Send request with data
        xhttp.send(formData);
-       if (response == 1) return (files[0].name);
-    }else{
-       alert("Please select a file");
     }
   
-  }
+}
+
+export function getDuration(src){
+  // Create a non-dom allocated Audio element
+  var au = document.createElement('audio');
+
+  // Define the URL of the MP3 audio file
+  au.src = "/view/assets/song/" + src;
+
+  // Once the metadata has been loaded, display the duration in the console
+  au.addEventListener('loadedmetadata', function(){
+      // Obtain the duration in seconds of the audio file (with milliseconds as well, a float value)
+      var duration = au.duration;
+
+      // example 12.3234 seconds
+
+      // alert("The duration of the song is of: " + duration + " seconds");
+      console.log("GAGAGAA");
+      return duration;
+      // Alternatively, just display the integer value with
+      // parseInt(duration)
+      // 12 seconds
+  },false); 
+}
