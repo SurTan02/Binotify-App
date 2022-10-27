0710968fda192fcf8c3f69e69e2116b25475217f
fikrikhoironn <fikrikf470@gmail.com>
2022-10-27 10:12:11 +0700

    Feat: nambahin navbar di daftar_album.php

--- src/daftar_album.php
+++ src/daftar_album.php
@@ -1,4 +1,10 @@
 <?php
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app//Song.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] .'/app/model/User.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/helper/cookies.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/helper/session.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/handlers/header_handler.php';
     $header_html = file_get_contents('./view/html/components/header_admin.html');
     
     $head_html = file_get_contents('./view/html/templates/head.html');
@@ -8,10 +14,19 @@
     $head_html = str_replace('{css2}', './view/css/search.css', $head_html);
     $head_html = str_replace('{css3}', './view/css/components/card_song.css', $head_html);
 
+    $idUser = validateAuthCookie($_COOKIE);
+
+    $user = new User();
+    $isAdmin = $user->getRoleById($idUser);
+    $username = $user->getUsernameById($idUser);
+
+    // if session["login"] is not set, use guest header
+    $isSetSession = isset($_SESSION["login"]);
+    $header_html = getHeader($isAdmin, $isSetSession, $username);
     $search_html = file_get_contents('./view/html/daftar_album.html');
 
     echo $head_html;
-    // echo $header_html;
+    echo $header_html;
     echo $search_html;
     echo '<script src="./view/js/daftar_album.js"></script>';
 ?>
