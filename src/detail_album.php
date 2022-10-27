f837aabad2c62e9fa015c9463d95f007b65288b2
Brianaldo <brianaldophandiarta31@gmail.com>
2022-10-27 12:27:21 +0700

    feat: admin access detail_album

--- src/detail_album.php
+++ src/detail_album.php
@@ -1,8 +1,22 @@
 <?php
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'] .'/app/model/User.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/helper/cookies.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/helper/session.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/app/handlers/header_handler.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/app/model/Song.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/app/model/Album.php';
 
-    $header_html = file_get_contents('./view/html/components/header_admin.html');
+    // Get the user's id.
+    $idUser = validateAuthCookie($_COOKIE);
+
+    $user = new User();
+    $isAdmin = $user->getRoleById($idUser);
+    $username = $user->getUsernameById($idUser);
+
+    // if session["login"] is not set, use guest header
+    $isSetSession = isset($_SESSION["login"]);
+    $header_html = getHeader($isAdmin, $isSetSession, $username);
     
     $head_html = file_get_contents('./view/html/templates/head.html');
     // Replace any variables in the html files.
@@ -29,14 +43,32 @@
             $detail_album_html = str_replace('{total_durasi}', gmdate('H:i:s', $album_result['total_duration']), $detail_album_html);
             $detail_album_html = str_replace('{album_genre}', $album_result['genre'], $detail_album_html);
             $detail_album_html = str_replace('{album_tanggal-terbit}', $album_result['tanggal_terbit'], $detail_album_html);
-    
+            
+            $admin_control = "";
+            if ($isAdmin) {
+                 $admin_control = $admin_control . '<div class="detail_album-action">
+                                                        <button onclick="save()">SAVE CHANGES</button>
+                                                        <button onclick="deleteAlbum()">DELETE ALBUM</button>
+                                                        <input
+                                                        type="file"
+                                                        id="image-input"
+                                                        accept="image/jpeg, image/png, image/jpg"
+                                                        />
+                                                    </div>';
+            } else {
+                $detail_album_html = str_replace('<a onclick="edit(this)">✎</a>', "", $detail_album_html);
+            }
+
+            $detail_album_html = str_replace('{admin_control}', $admin_control, $detail_album_html);
             $song_list = '';
     
             foreach ($songs_result as $idx=>$song) {
                 $song_list = $song_list . 
                 '<tr id="song_'. $song['song_id'] .'">
-                    <td style="text-align: center" class="number-container">
-                        <span class="remove" onclick="removeSong('. $song['song_id'] .')">
+                    <td style="text-align: center" class="number-container">';
+                
+                if ($isAdmin) {
+                    $song_list = $song_list . '<span class="remove" onclick="removeSong('. $song['song_id'] .')">
                         <svg
                             fill="#000000"
                             xmlns="http://www.w3.org/2000/svg"
@@ -50,8 +82,12 @@
                             />
                         </svg>
                         </span>
-                        <span class="number">'. $idx + 1 .'</span>
-                    </td>
+                        <span class="number">'. $idx + 1 .'</span>';
+                } else {
+                    $song_list = $song_list . '<span>'. $idx + 1 .'</span>';
+                }
+
+                $song_list = $song_list . '</td>
                     <td style="display: flex; flex-direction: column; gap: 8px">
                         <p class="judul_lagu">'. $song['judul'] .'</p>
                         <p>'. $song['penyanyi'] .'</p>
@@ -62,13 +98,15 @@
             
             $detail_album_html = str_replace('{song-list}', $song_list, $detail_album_html);
 
-             echo $head_html;
-            // echo $header_html;
+            echo $head_html;
+            echo $header_html;
             echo $detail_album_html;
             echo '<script src="./view/js/detail_album.js"></script>';
         } else {
             // load page not found
             echo $head_html;
+            echo $header_html;
+            echo 404;
         }
     }
 
