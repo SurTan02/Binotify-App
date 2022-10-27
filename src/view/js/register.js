e05c8db2332c161aeb134bbee5b0a7646d489901
fikrikhoironn <fikrikf470@gmail.com>
2022-10-27 20:13:14 +0700

    fix: register harusnya dia gabisa submit kalo masih merah, tapi ini debounce bikin susah dah

--- src/view/js/register.js
+++ src/view/js/register.js
@@ -1,3 +1,13 @@
+/**
+ * init isInputValid object
+ * 
+ */
+const isInputValid = {
+  "username": false,
+  "email": false,
+  "password": false,
+};
+
 // create debounce function
 const debounce = (func, delay) => {
   let inDebounce;
@@ -11,37 +21,12 @@ const debounce = (func, delay) => {
 
 
 /**
- * init isInputValid object
- * 
- */
-const isInputValid = {
-  "username": false,
-  "email": false,
-  "password": false,
-};
-
-
-/**
- * validateInpit is a function that validates the input of the user is valid
- * 
- * @returns {boolean} true if all inputs are valid
- */
-function validateInput() {
-  for (let key in isInputValid) {
-    if (isInputValid[key] === false) {
-      return false;
-    }
-  }
-  return true;
-}
-
-/**
  * using ajax to send the data to the server to validate is the username is valid and manipulate the DOM
  */
 
 let input_username = document.getElementById("username");
 let error_username = document.getElementById("error_username");
-input_username.addEventListener("keyup", debounce(function () {
+input_username.addEventListener("change", debounce(function () {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -66,7 +51,7 @@ input_username.addEventListener("keyup", debounce(function () {
         }
       }
       else {
-        isInputValid.username = false;
+        isInputValid["username"] = false;
         input_username.style.borderColor = "#ff0000";
         error_username.innerHTML = "Username must contain only letters, numbers and underscore";
         error_username.style.color = "#ff0000";
@@ -76,9 +61,11 @@ input_username.addEventListener("keyup", debounce(function () {
       }
     }
   }
+  console.log(isInputValid);
+  validateInput();
   xhr.open("GET", "./view/js/ajax/register.php?username=" + input_username.value, true);
   xhr.send();
-}, 500));
+}, 0));
 
 
 /**
@@ -87,7 +74,7 @@ input_username.addEventListener("keyup", debounce(function () {
  */
 let input_email = document.getElementById("email");
 let error_email = document.getElementById("error_email");
-input_email.addEventListener("keyup", debounce(function () {
+input_email.addEventListener("change", debounce(function () {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -120,9 +107,13 @@ input_email.addEventListener("keyup", debounce(function () {
       }
     }
   }
+  console.log(isInputValid);
+  validateInput();
+
+
   xhr.open("GET", "./view/js/ajax/register.php?email=" + input_email.value, true);
   xhr.send();
-}, 500));
+}, 0));
 
 /**
  * validate is the password same as the confirm password and manipulate the DOM
@@ -132,7 +123,7 @@ let input_password = document.getElementById("password");
 let input_password_confirm = document.getElementById("password_confirm");
 let error_password = document.getElementById("error_password");
 let error_password_confirm = document.getElementById("error_password_confirm");
-input_password_confirm.addEventListener("keyup", debounce(function () {
+input_password_confirm.addEventListener("change", debounce(function () {
   let same = false;
   if (input_password_confirm.value === input_password.value) {
     same = true;
@@ -148,5 +139,29 @@ input_password_confirm.addEventListener("keyup", debounce(function () {
     error_password_confirm.style.fontFamily = "Inter";
     error_password_confirm.style.fontSize = "12px";
     error_password_confirm.style.fontStyle = "oblique";
+    isInputValid["password"] = false;
+
   }
-}, 500));
+  console.log(isInputValid);
+  validateInput();
+
+
+}, 0));
+
+
+/**
+ * validateInpit is a function that validates the input of the user is valid
+ * 
+ * @returns {boolean} true if all inputs are valid
+ */
+function validateInput() {
+  console.log("ngecek is input valid");
+  let status = true;
+  for (const key in isInputValid) {
+    if (isInputValid[key] === false) {
+      status = false;
+    }
+  }
+  let submit_button = document.getElementById("register_button");
+  status ? submit_button.disabled = false : submit_button.disabled = true;
+}
